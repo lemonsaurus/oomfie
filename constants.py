9663b6b8f21ab3c24a73ba599e3cc8b4799049e3
Leon Sandøy <2098517+lemonsaurus@users.noreply.github.com>
2023-06-20 13:21:05 +0200

    Add a constants file, with officer rIDs.

---
+++ constants.py
@@ -0,0 +1,11 @@
+"""
+This file keeps various constants we may need to use many places in the bot.
+
+It should not contain constants that will only be used in a single place in the bot, 
+as these are far better off simply being created inside the file where they'll be used.
+
+Keep in mind the locality principle.
+"""
+
+# Role IDs for the Oomfie server
+OFFICERS = [1095313833507164272]
